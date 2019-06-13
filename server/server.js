363dab8b98c3804f2f4eb7a980d66a0eac164658
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2019-06-13 20:05:57 +0100

    new commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require("express");
 const publicPath = path.join(__dirname, '../public');
-
+let port = process.env.PORT || 3000
 let servingDir = express.static(publicPath);
 let app = express();
 app.use(servingDir);
 // app.get("/", (req, res)=>{
 //     res.send("listening bro");
 // });
-app.listen(process.env.PORT || 3000);
+app.listen(port);
 
